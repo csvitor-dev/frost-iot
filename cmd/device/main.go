b856784086c772441129e057f1b142a4ceca3a71
csvitor-dev <csvitor.dev@gmail.com>
2025-01-08 12:43:13 -0300

    sample: an example of 'instantiation' of the device

--- cmd/device/main.go
+++ cmd/device/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/csvitor-dev/frost-iot/src/device"
+)
+
+func main() {
+	d, err := device.NewRefrigeratorDevice(8.0, 0.1)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(d)
+
+	d.SetPortState()
+	fmt.Println(d)
 }
